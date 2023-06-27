014c3ad77e628fcb80d558ada3964136f1190ec3
mrodriguezdefreitas <mrodriguezdefreitas@gmail.com>
2023-06-27 19:38:07 -0300

    mi primer commit con react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  // aca va todo lo de JS
+  let saludo = "bienvenidos a mi web"
+  function hacerClick(){
+console.log("usted hizo click")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    {saludo}
+    <button onClick={hacerClick}> hace click </button>
+   </div>
+ 
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import "./Header.css"
+function Header(){
+    return(
+        <header>
+            <h2>logo</h2>
+            <nav>
+                <a href="">Home</a>
+                <a href="">Shop</a>
+                <a href="">Contacto</a>
+            </nav>
+        </header>
+    )
+}
+export default Header
